b882e21cdc85c83cd830028dd8d14fcc99f388c4
xrinu <rinuxavier96@gmail.com>
2020-06-01 16:40:28 -0400

    finished 4 and 5

---
+++ script.js
@@ -0,0 +1,6 @@
+//4) Select a list item with a class of third, but only the list item inside of the ol tag.
+document.getElementById("container").querySelector('section ol .third')
+
+//5) Give the section with an id of container the text "Hello!".
+var message=document.getElementById('container')
+message.append('Hello!')
