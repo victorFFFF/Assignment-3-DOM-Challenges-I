1ab1d3d926a43275fb646687752ef553ba536033
xrinu <rinuxavier96@gmail.com>
2020-06-01 16:46:25 -0400

    finished 6

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ document.getElementById("container").querySelector('section ol .third')
 //5) Give the section with an id of container the text "Hello!".
 var message=document.getElementById('container')
 message.append('Hello!')
+
+//6) Add the class main to the div with a class of footer.
+const ans6 = document.getElementsByClassName("footer")[0].classList.add("main");
+console.log(ans6);
