ec75e749dfb7ea70ee22da41210b5897c3b7aeac
Victor Li <victor.li.1995@hotmail.com>
2020-06-01 16:30:18 -0400

    waiting for 1-9

--- script.js
+++ script.js
@@ -10,13 +10,26 @@ console.log(theContainer2)
 let allSecond = document.querySelectorAll(".second")
 console.log(allSecond)
 
+//Quesiton4
+let i =document.getElementById("container").querySelector('section ol .third')
+console.log(i)
+
+//Question5
+let para = document.createElement("p");
+let paraNode = document.createTextNode("Hello")
+para.appendChild(paraNode)
+let existing = document.getElementById("container")
+existing.appendChild(paraNode) 
+
+
+
 //Question 10
 let node = document.createElement("LI");                 // Create a <li> node
 let textnode = document.createTextNode("Water");         // Create a text node
 node.appendChild(textnode);                              // Append the text to <li>
 document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
 
-//Question 12
+//Question 11
 let theOrderList = document.getElementById("orderList")
 let items = theOrderList.getElementsByTagName("li")
 for(let i  =0; i < items.length; i++)
@@ -24,7 +37,7 @@ for(let i  =0; i < items.length; i++)
     document.getElementById('orderList').style.backgroundColor = "green"
 }
 
-//Question 13
+//Question 12
 var ele = document.getElementById("myDiv")
 ele.remove();
   
