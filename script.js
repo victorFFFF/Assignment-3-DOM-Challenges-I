94aafa38487d78130c00bb5c160f12f22dd9ce6b
Victor Li <victor.li.1995@hotmail.com>
2020-06-01 14:22:11 -0400

    finish question 11

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var node = document.createElement("LI");                 // Create a <li> node
-var textnode = document.createTextNode("Water");         // Create a text node
+//Question 10
+let node = document.createElement("LI");                 // Create a <li> node
+let textnode = document.createTextNode("Water");         // Create a text node
 node.appendChild(textnode);                              // Append the text to <li>
 document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+//Question 11
+let theOrderList = document.getElementById("orderList")
+let items = theOrderList.getElementsByTagName("li")
+for(let i  =0; i < items.length; i++)
+{
+    document.getElementById('orderList').style.backgroundColor = "green"
+}
