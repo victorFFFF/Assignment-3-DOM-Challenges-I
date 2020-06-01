d7f082dabf0743be5a8b661fff3ae0d7884ab4c1
Victor Li <victor.li.1995@hotmail.com>
2020-06-01 15:33:58 -0400

    Finish q1-3 & q10-13

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Question 1
+let theContainer = document.getElementById('container')
+console.log(theContainer)
+
+//Question 2
+let theContainer2 = document.querySelector("#container")
+console.log(theContainer2)
+
+//Question 3
+let allSecond = document.querySelectorAll(".second")
+console.log(allSecond)
+
 //Question 10
 let node = document.createElement("LI");                 // Create a <li> node
 let textnode = document.createTextNode("Water");         // Create a text node
@@ -11,3 +23,8 @@ for(let i  =0; i < items.length; i++)
 {
     document.getElementById('orderList').style.backgroundColor = "green"
 }
+
+//Question 12
+var ele = document.getElementById("myDiv")
+ele.remove();
+  
