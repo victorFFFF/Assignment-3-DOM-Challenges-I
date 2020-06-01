7f61489a1772562c769852cf534d7890e698dedc
Victor Li <victor.li.1995@hotmail.com>
2020-06-01 13:57:42 -0400

    created script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var node = document.createElement("listOrder");                 // Create a <li> node
+var textnode = document.createTextNode("Water");         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
